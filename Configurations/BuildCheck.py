e52b69b821ecf9936784062ca137710ca4a706cf
BarunPrakash <brnprksh@gmail.com>
2019-09-02 17:49:45 +0530

    Build Managements!!
    This  is going to tell  you no of line code on respectively date.
    Directories :- no of .c /.cpp /.h/Makefile  inside components.

---
+++ Configurations/BuildCheck.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+# python script for  counting the number of line print date /Time/
+#productivity Trackers
+#Designed By BARUN 
+#Date  3 Sept 2019
+########################################################################3
+
+
+num_lines = sum(1 for line in open('BankingPro.cpp'))
+print (num_lines)
